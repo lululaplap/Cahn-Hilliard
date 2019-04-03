96cc6ee48b645f9dcd888c1eefb51da2860b9dbb
Lewis <lewis@lappin.me.uk>
2019-04-03 12:54:05 +0100

    works

--- README.md
+++
@@ -1,1 +0,0 @@
-# Cahn-Hilliard
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import copy
 fig = plt.figure()
 #fig2 = plt.figure()
 
-ax1 = fig.add_subplot(1,1,1)
-#ax2 = fig.add_subplot(2,1,2)
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
 
 
 
 class Cahn():
-    def __init__(self,N):
+    def __init__(self,N,M=0.1,dx=1,dt=1,k=0.1,a=0.1):
         self.N = N
         p=0.5
         self.M = 0.1
@@ -30,19 +30,16 @@ class Cahn():
         print(self.laplace)
         #phi = np.ones((self.N,self.N))*0.5+np.random.rand(self.N,self.N)-np.random.rand(self.N,self.N)
         #phi = np.zeros((self.N,self.N))+np.random.rand(self.N,self.N)/100-np.random.rand(self.N,self.N)/100
-        phi = np.random.uniform(-0.01,0.01,size=(self.N,self.N))
-        #phi = np.random.choice(a=[-0.5,0.5], size=(N, N), p=[p, 1-p])
+        #phi = np.random.uniform(-0.01,0.01,size=(self.N,self.N))
+        phi = np.random.choice(a=[-0.5,0.5], size=(N, N), p=[p, 1-p])
         self.phi = phi
         self.F = []
 
     def getMu(self):
         phi_old = copy.deepcopy(self.phi)
         l = signal.convolve2d(phi_old,self.laplace, mode="same",boundary = "wrap")
-        a = -self.a*phi_old
-        b =  self.a*(np.power(phi_old,3))
-        c =  -self.k*l
-        t = a+b+c
-        return t
+        return -self.a*phi_old + self.a*(np.power(phi_old,3))  -self.k*l
+
     def getPhi(self):
         mu = self.getMu()
         l = signal.convolve2d(mu,self.laplace, mode="same",boundary = "wrap")
@@ -51,9 +48,9 @@ class Cahn():
 
     def getf(self):
         #g = signal.convolve2d(self.phi,self.grad, mode="same",boundary = "wrap")
-        g = filter.sobel(self.phi,mode='wrap')
-        #xg,yg = np.gradient(self.phi)
-        #g = np.sqrt(np.power(xg,2)+np.power(yg,2))
+        #g = filter.sobel(self.phi,mode='wrap')
+        xg,yg = np.gradient(self.phi)
+        g = np.sqrt(np.power(xg,2)+np.power(yg,2))
         return -1*((self.a/2)*np.power(self.phi,2)) + self.a/4*np.power(self.phi,4) + (self.k/2 * self.norm* (np.power(g,2)))
     def update(self,i):
         for i in range(1000):
@@ -63,14 +60,11 @@ class Cahn():
         ax1.clear()
         ax1.imshow(self.phi, interpolation='nearest', cmap = 'cool', vmin=-1, vmax=1, origin='lower')
         plt.title(i)
-        #ax2.clear()
-        #ax2.plot(self.F)
-        #lt.axis(option='square')
-
-        #return self.phi
+        ax2.clear()
+        ax2.plot(self.F)
 
 def main():
-    C = Cahn(100)
+    C = Cahn(500)
     ani = animation.FuncAnimation(fig, C.update)
     plt.show()
     plt.plot(C.F)
